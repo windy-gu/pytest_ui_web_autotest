74ca5517994ae125d55998e8d9e60b8fa485b3aa
windy-gu <408563133@qq.com>
2021-07-06 17:00:37 +0800

    增加循环判断处理超过10条的记录数据

--- test_dir/test_001_a_oa.py
+++ test_dir/test_001_a_oa.py
@@ -25,9 +25,13 @@ def test_oa_login(browser, url='https://oa.wownow.net/wui/index.html#/?logintype
                   account='guxiaofeng', psw='gxf4843860.'):
     page = OAPage(browser)
     page.get(url)
+
+    # login
     page.login_name = account
     page.login_password = psw
     page.login_btn.click()
+
+    # 进入：流程 - 我的请求 - S-app 加班申请
     page.door.click_and_hold()
     time.sleep(1)
     page.liucheng.click()
@@ -35,45 +39,49 @@ def test_oa_login(browser, url='https://oa.wownow.net/wui/index.html#/?logintype
     time.sleep(1)
     page.my_request.click()
     page.overtime_application.click()
-    page.search_input.send_keys("2020")
+
+    # 查询指定条件的加班申请&获取符合条件的数据
+    page.search_input.send_keys("2021-06")
     page.search_input.send_keys(Keys.ENTER)
     list_int = int(str(page.list_operator_count.get_attribute("textContent"))[1: -1])
-
     time.sleep(1)
-    get_info_from_list(browser, list_int=list_int)
+
+    operator_by_list(browser, list_int=list_int)
     time.sleep(3)
 
 
-def get_info_from_list(browser, list_int: int):
+def operator_by_list(browser, list_int: int):
     """
 
     :param browser:
     :param list_int:
     :return:
     """
-    page = OAPage(browser)
-    need_list = []
     hour = 0
-    outside_loop = int(list_int/10)
-    inside_loop = list_int % 10
-
-    print(outside_loop)
-    print(inside_loop)
+    # 根据查询出来的数据，自动处理需要进行循环次数和翻页次数
+    outside_loop = int(list_int/10)  # 翻页次数
+    inside_loop = int(list_int % 10)  # 单页循环次数
 
     if outside_loop == 0:
-        hour = hour + get_info(browser, loop=inside_loop)
+        hour = hour + get_info_by_detail(browser, loop=inside_loop)
         # pass
     else:
         for i in range(outside_loop+1):
             if i < outside_loop:
-                hour = hour + get_info(browser, loop=10)
+                hour = hour + get_info_by_detail(browser, loop=10)
 
             else:
-                hour = hour + get_info(browser, loop=inside_loop)
+                hour = hour + get_info_by_detail(browser, loop=inside_loop)
     print("加班工时：" + str(hour))
 
 
-def get_info(browser, loop: int):
+def get_info_by_detail(browser, loop: int):
+    """
+
+    :param browser:
+    :param loop:
+    :return:
+    """
     hour = 0
     for n in range(loop):
         xpath = '//tbody[@class="ant-table-tbody"]/tr[' + str(n + 1) + ']/td[2]'
@@ -87,25 +95,27 @@ def get_info(browser, loop: int):
             else:
                 browser.switch_to_window(all_handles[i])
         time.sleep(1)
+
+        # 获取名称和加班时长数据
         person_xpath = '//tbody/tr[5]/td[4]/div[1]'
         overtime_long = '//tbody/tr[11]/td[4]/div[1]'
-        overtime_date = '//tbody/tr[13]/td[4]/div[1]'
+        # overtime_date = '//tbody/tr[13]/td[4]/div[1]'
 
         name = NewPageElement(xpath=person_xpath).text
         time.sleep(1)
         long = NewPageElement(xpath=overtime_long).text
         time.sleep(1)
-        date = NewPageElement(xpath=overtime_date).text
+        # date = NewPageElement(xpath=overtime_date).text
         # need_list.append(name + '_' + long + '_' + date)
         hour = hour + float(long)
-        # print(need_list)
 
+        # 关闭detail页面切换回到list页面
         browser.close()
         time.sleep(1)
         browser.switch_to_window(old_handle)
         time.sleep(1)
 
-        return float(long)
+    return float(hour)
 
 
 if __name__ == '__main__':
