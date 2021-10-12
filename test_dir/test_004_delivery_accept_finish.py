7961c3410a37d7682b3ef69153d7bc2e85726498
windy-gu <408563133@qq.com>
2021-10-12 16:55:30 +0800

    补充待完成订单的判断流程

--- test_dir/test_004_delivery_accept_finish.py
+++ test_dir/test_004_delivery_accept_finish.py
@@ -34,12 +34,12 @@ def test_replace_accept_finish(browser,
     # 判断当前是否在外卖管理 - 订单管理 - 外卖订单页面
     if boss_delivery_order_url != browser.current_url:
         page_delivery_management.get(boss_delivery_order_url)
-        # browser.dismiss_alert()
-        time.sleep(3)
+        time.sleep(2)
+
     if boss_delivery_order_url == browser.current_url:
         # 进入到：等待门店接单流程
         page_delivery_management.wait_accept_order_tab.click()
-        time.sleep(3)
+        time.sleep(2)
 
         wait_ords = get_store_wait_accept_ords(store_no, biz_state='10')
         accept_flag = False
@@ -60,49 +60,52 @@ def test_replace_accept_finish(browser,
                     page_delivery_management.confirm_button.click()
                     accept_flag = True
         else:
-            log.info('当前门店：{} 无 待接单的订单'.format(store_no))
+            log.warn('当前门店：{} 无 待接单的订单'.format(store_no))
 
         if accept_flag:
             finish_ords = get_store_wait_accept_ords(store_no, biz_state='11')
             page_delivery_management.accept_order_tab.click()
+            if len(finish_ords) >= 1:
+                for i in range(len(finish_ords)):
+                    log.print('完成订单：第 {} 次'.format(str(i+1)))
+                    page_delivery_management.order_no_input.clear()
+                    page_delivery_management.order_no_input = finish_ords[i]
+                    page_delivery_management.search_btn.click()
+                    time.sleep(2)
 
-            for i in range(len(finish_ords)):
-                log.print('完成订单：第 {} 次'.format(str(i+1)))
-                page_delivery_management.order_no_input.clear()
-                page_delivery_management.order_no_input = finish_ords[i]
-                page_delivery_management.search_btn.click()
-                time.sleep(3)
-
-                # 判断操作list区域元素是否存在
-                if page_delivery_management.opera_list.check_element():
-                    page_delivery_management.ord_finish_list.click()
-                    time.sleep(1)
-                    if i == 0:
-                        page_delivery_management.finish_selected_span.click()
-                        page_delivery_management.finish_select_others_reason_li.click()
-                    page_delivery_management.finish_remark_input = 'ui test'
-                    page_delivery_management.confirm_button.click()
-                    time.sleep(1)
+                    # 判断操作list区域元素是否存在
+                    if page_delivery_management.opera_list.check_element():
+                        page_delivery_management.ord_finish_list.click()
+                        time.sleep(1)
+                        if i == 0:
+                            page_delivery_management.finish_selected_span.click()
+                            page_delivery_management.finish_select_others_reason_li.click()
+                        page_delivery_management.finish_remark_input = 'ui test'
+                        page_delivery_management.confirm_button.click()
+                        time.sleep(1)
+            else:
+                log.warn('当前门店：{} 无 待完成的订单'.format(store_no))
 
 
 def get_store_wait_accept_ords(store_no: str, biz_state: str):
     """
     获取指定门店 - 待接单的订单号
     :param store_no:门店标号
-    :param biz_state:订单状态
+    :param biz_state:订单状态，10：待门店接单
     :return:订单号，可以为空
     """
     mysql = MySQL()
     ords = mysql.select(
                  "SELECT order_no FROM `lifekh_takeaway_uat`.`takeaway_order`"
                  "WHERE `store_no` = '{store_no}' "
-                 "AND `biz_state` = '{biz_state}' ORDER BY `update_time` DESC ".format(store_no=store_no, biz_state=biz_state))
+                 "AND `biz_state` = '{biz_state}' ORDER BY `update_time` DESC "
+                 .format(store_no=store_no, biz_state=biz_state))
     ords_list = []
     if isinstance(ords, list):
         for i in range(len(ords)):
             ords_list.append(ords[i]['order_no'])
     else:
-        log.info('门店：{},当前无待接单订单'.format(store_no))
+        log.warn('门店：{store_no},当前无biz_state：{biz_state} 订单'.format(store_no=store_no, biz_state=biz_state))
     return ords_list
 
 
